d1d19887d5699360dc0e32838719df7c2bab69d6
Danilo Tiago <backendengineer.ark@gmail.com>
2023-11-08 00:08:28 -0300

    corrected nullpointer task routine

--- src/app/core/models/routine.model.ts
+++ src/app/core/models/routine.model.ts
@@ -2,7 +2,7 @@ import { Task } from "./task.model";
 
 export class Routine {
     id: string;
-    task: Task
+    task: Task;
     reference_date: string;
     is_finished: boolean;
     created_at: Date;
@@ -10,7 +10,9 @@ export class Routine {
 
     fromJson(json: any) {
         this.id           = json.id;
-        this.task = new Task().fromJson(json.task)
+        if (json.task) {
+            this.task = new Task().fromJson(json.task);
+        }
         this.reference_date     = json.reference_date;
         this.is_finished = json.is_finished
         this.created_at    = new Date(json.created_at);
